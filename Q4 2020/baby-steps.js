407a8e2a9366df3001e4eebf36b2fef89ef67ed9
meta-udeshay-pareek <46710014+meta-udeshay-pareek@users.noreply.github.com>
2021-01-13 09:14:11 +0530

    Delete Q4 2020 directory

--- Q4 2020/baby-steps.js
+++
@@ -1,5 +0,0 @@
-sum = 0;
-for (i = 2; i < process.argv.length; i++) {
-    sum += Number(process.argv[i]);
-}
-console.log(sum);
--- Q4 2020/make-it-modular.js
+++
@@ -1,9 +0,0 @@
-require('./mymodule')(process.argv[2], process.argv[3], function (error, list) {
-	if (error) {
-		return console.log(error)
-	}
-
-	list.forEach(function (entry) {
-		console.log(entry)
-	})
-})
